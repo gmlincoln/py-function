7624ac4e38f1166ed22c733f858a6f41ba21a225
Md Golam Maula <maula03@gmail.com>
2025-11-23 22:39:53 +0600

    class-27 added

--- function.py
+++ function.py
@@ -9,6 +9,13 @@
     
 # sum()
 
+# def greet(name = "Student"):
+#     print(f"Hello {name}")
+    
+    
+# name = input("Enter your name: ")
+# greet(name)
+# greet()
 
 
 # def sum(a, b):
@@ -35,3 +42,55 @@
 
 # print(num1, num2)
 
+# a = 10
+# b = 10
+# c = "+"
+
+# if c == "+":
+#     print(a + b)
+
+# print(result)
+
+
+# def calculator(a, b, operator):
+#     if operator == "+":
+#         return a + b
+#     elif operator == "-":
+#         return a - b
+#     elif operator == "*":
+#         return a * b 
+#     elif operator == "/":
+#         return a / b
+    
+
+# n1, n2 = map(int, input("Enter two values: ").split())
+# operator = input("Operator: ")
+    
+# result  = calculator(n1, n2, operator)
+
+# print(result)
+
+
+
+# def add(*args):
+#     print(sum(args))
+
+# add(5, 10, 15)
+
+
+# def newFunc(**kargs):
+#     print(kargs)
+    
+
+
+# newFunc(name="Riyad", age=20, isMarried = False, cgpa = 3.87, dept = "CSE")
+
+
+def outer():
+    print("Outer Function")
+    def inner():
+        print("Inner Function")
+    inner()
+       
+        
+outer()
