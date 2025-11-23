0fe8aea7c21ee4b40c948af2c9900c96ee2fb981
Md Golam Maula <maula03@gmail.com>
2025-11-23 11:03:42 +0600

    class-27 added

---
+++ function.py
@@ -0,0 +1,37 @@
+# def greetings():
+#     print("Hello World!")
+    
+
+# greetings()
+
+# def sum():
+#     print(5+10)
+    
+# sum()
+
+
+
+# def sum(a, b):
+#     return a + b
+
+# result = sum(10, 10)
+# result2 = sum(100, 10)
+
+# print(result)
+# print(result2)
+
+
+def sum(a, b):
+    return a + b
+
+num1, num2 = map(int, input("Enter two values: ").split())
+
+# print(sum(num1, num2))
+result = sum(num1, num2)
+
+print(result)
+
+
+
+# print(num1, num2)
+
